da04424b2dcf65fa7c0dccb884cbc02f1570b391
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-02-05 11:51:02 +0700

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "Name" : "Sandika",
+      "Age" : "22",
+      "favColor" : ["Black", "Red", "Amber"]
+    },
+    {
+      "Name" : "Rahmat",
+      "Age" : "23",
+      "favColor" : ["Green", "Red", "Amber"]
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+       home: Scaffold(
+        appBar: AppBar(
+          title: Text('ID Apps'),
+        ),
+        body: ListView(
+          children: myList.map((data) {
+            return Card(
+              child: Column(
+                children: [
+                  
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
