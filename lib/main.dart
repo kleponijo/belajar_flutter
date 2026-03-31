6e61d38917d9c91f11eea080beb712125e2658d1
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-03-31 09:53:29 +0700

    Update main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Belajar Widget dasar
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Apps"),
+        ),
+        body: Center(
+          child: Text("Body"),
+        ),
+      ),
+    );
+  }
+}
