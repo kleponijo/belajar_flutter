23c71d57efc749c672039190d155bf09264269bf
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-04-02 15:09:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Hello World")),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(child: Text("Hello World")),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
