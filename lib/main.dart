4389c39cdad6e8f769d0197895d1f686823e6ca2
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-04-02 14:49:18 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,3 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final List<Color> myColor = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.amber,
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("List View", style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-        ),
-        body: ListView.separated(
-          separatorBuilder: (context, index) {
-            return Divider(color: Colors.black);
-          },
-          itemCount: myColor.length,
-          itemBuilder: (context, index) {
-            return Text("Hallo");
-          },
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MyApp());
