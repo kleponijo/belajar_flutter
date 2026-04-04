7765171bfa92df1213a15e022871fd65183a5308
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-04-04 18:07:13 +0700

    ooke

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Hello World")),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(child: Text("Hello World")),
-        backgroundColor: Colors.white,
-      ),
+      debugShowCheckedModeBanner: false, 
+      theme: ,
+      home: MyHomePage()
     );
   }
 }
