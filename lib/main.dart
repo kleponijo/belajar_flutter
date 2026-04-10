5e1d6c22f6c1a2b72274ee06a1011587eb33015e
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-04-10 20:35:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as myhttp;
-
 void main() {
-  runApp(MyApp());
+  cetakB().then((value) => "berhasil").catchError((err) => (err));
+  print("A");
+  print("C");
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<String> cetakB() async {
+  // Future();
+  await Future.delayed(Duration(seconds: 2));
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false);
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("HTTP")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("data", style: TextStyle(fontSize: 20)),
-            SizedBox(height: 15),
-            ElevatedButton(onPressed: () {myhttp.get(url)}, child: Text("Get Data")),
-          ],
-        ),
-      ),
-    );
-  }
+  return "Berhasil cetak b";
 }
